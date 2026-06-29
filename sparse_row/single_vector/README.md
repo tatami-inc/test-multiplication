a3b8dd3a7020d4d7ebea259d46f5efb1409f16cc
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-06-29 18:34:43 +1000

    Added perf tests for sparse matrix x single vector.

---
+++ sparse_row/single_vector/README.md
@@ -0,0 +1,100 @@
+# Sparse row-major product with a single vector
+
+## Strategies
+
+All strategies must iterate across consecutive rows of the LHS matrix in the outermost loop.
+We will be loading rows on demand via the **tatami** interface, so the full matrix will not be available for random access.
+
+### Naive
+
+For each LHS row, we compute the dot product against the RHS vector.
+
+### Accumulators
+
+This is the same as the naive approach, except that the dot product is computed with multiple accumulators.
+The idea is to break dependency chains in the CPU's instruction pipeline by allowing multiple accumulations to occur in parallel.
+It also provides some opportunities for auto-vectorization of the sum.
+
+### Comments on blocking 
+
+In theory, we could try to re-use a block of the RHS vector across multiple LHS rows, to avoid having to reload it from memory when computing the dot product.
+The problem is that, for fixed-size blocks, the blocking overhead would be comparable to the actual calculations within each block when the data is sparse.
+In fact, the overhead would be larger than the calculations themselves if the block size is smaller than the inverse of the density.
+Additionally, we can't easily determine where to restart the innermost loop for each new block of LHS rows.
+Doing so would require either a binary search or extra tracking of the positions of the last non-zero element for each sparse row/column in the preceding block.
+
+We could consider using variable-size "blocks" of multiple LHS rows, which contain no more than a certain number of non-zero elements from each RHS row/column.
+This would improve the efficiency of traversal along the sparse rows by making the overhead (mostly) proportional to the number of non-zero elements processed.
+However, different LHS rows have different number of structural non-zeros, so towards the end of each row, we'd be wasting iterations on rows that have no more non-zeros.
+Additionally, for each sparse "block" of this nature, we would still need to access the union of all indices of the structural non-zeros in the dense RHS vector.
+The non-zero values can be arbitrarily distributed so we might end up using the entirety of the RHS vector. 
+
+## Instructions
+
+To build the test executable, use the usual CMake process:
+
+```sh
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+```
+
+## Double-precision results
+
+Trying out a range of matrix shapes.
+All timings below are obtained on an Intel i7-8850H.
+
+```console
+$ ./build/test -r 20000 -c 1000
+Results for 20000 x 1000
+naive                                             : 0.00230701 ± 1.29069e-05
+2 accumulators                                    : 0.00180446 ± 2.35341e-06
+4 accumulators                                    : 0.00171246 ± 7.20213e-06
+8 accumulators                                    : 0.00171252 ± 7.68304e-06
+
+$ ./build/test -r 5000 -c 10000
+Results for 5000 x 10000
+naive                                             : 0.00613053 ± 1.03335e-05
+2 accumulators                                    : 0.00463167 ± 1.42148e-05
+4 accumulators                                    : 0.00444495 ± 8.76551e-06
+8 accumulators                                    : 0.00441638 ± 6.97368e-06
+
+$ ./build/test -r 1000 -c 50000
+Results for 1000 x 50000
+naive                                             : 0.00683221 ± 1.23612e-05
+2 accumulators                                    : 0.00658741 ± 8.13006e-06
+4 accumulators                                    : 0.00623488 ± 9.91883e-06
+8 accumulators                                    : 0.00624136 ± 1.04279e-05
+```
+
+## Single-precision results
+
+Again, but with single-precision floats.
+
+```console
+$ ./build/test_float -r 20000 -c 1000
+Results for 20000 x 1000
+naive                                             : 0.0021056 ± 5.78579e-06
+2 accumulators                                    : 0.00153783 ± 5.90938e-06
+4 accumulators                                    : 0.00129449 ± 5.0177e-06
+8 accumulators                                    : 0.00129928 ± 8.42645e-06
+
+$ ./build/test_float -r 5000 -c 10000
+Results for 5000 x 10000
+naive                                             : 0.00575126 ± 0.000104191
+2 accumulators                                    : 0.00377424 ± 2.49744e-05
+4 accumulators                                    : 0.00345115 ± 2.39759e-05
+8 accumulators                                    : 0.00343676 ± 3.81891e-05
+
+$ ./build/test_float -r 1000 -c 50000
+Results for 1000 x 50000
+naive                                             : 0.00564083 ± 6.63435e-06
+2 accumulators                                    : 0.00449082 ± 1.26535e-05
+4 accumulators                                    : 0.00407817 ± 1.24714e-05
+8 accumulators                                    : 0.00407757 ± 7.91102e-06
+```
+
+## Conclusion
+
+Multiple accumulators provide a modest improvement to performance, consistent with conclusions from other projects ([IRLBA](https://github.com/libscran/irlba), and Eigen itself).
+This is most apparent when the shared dimension extent is high, though it's also not too bad when the shared dimension is small.
+It seems that 4 accumulators is typically the sweet spot.
---
+++ sparse_row/single_vector/test.cpp
@@ -0,0 +1,160 @@
+#include <random>
+#include <vector>
+#include <iostream>
+#include <cstddef>
+#include <array>
+
+#include "CLI/CLI.hpp"
+#include "eztimer/eztimer.hpp"
+
+#ifndef USE_SINGLE
+#define FLOAT double
+#else
+#define FLOAT float
+#endif
+
+template<int num_acc_, class Values_, class Indices_, class Dense_>
+FLOAT dense_sparse_dot_product(const Values_& values, const Indices_& indices, const Dense_& dense) {
+    const std::size_t num_non_zeros = values.size();
+
+    if constexpr(num_acc_ == 1) {
+        FLOAT dot = 0;
+        for (std::size_t i = 0; i < num_non_zeros; ++i) {
+            dot += dense[indices[i]] * values[i];
+        }
+        return dot;
+
+    } else {
+        std::array<FLOAT, num_acc_> dots{};
+        const std::size_t cycles = num_non_zeros / num_acc_;
+        const std::size_t remainder = num_non_zeros % num_acc_;
+
+        for (std::size_t c = 0; c < cycles; ++c) {
+            for (std::size_t i = 0; i < num_acc_; ++i) {
+                const auto idx = c * num_acc_ + i;
+                dots[i] += dense[indices[idx]] * values[idx]; 
+            }
+        }
+
+        FLOAT extras = 0;
+        for (std::size_t i = 0; i < remainder; ++i) {
+            const auto idx = cycles * num_acc_ + i;
+            extras += dense[indices[idx]] * values[idx];
+        }
+        return std::accumulate(dots.begin(), dots.end(), extras);
+    }
+}
+
+int main(int argc, char ** argv) {
+    CLI::App app{"Dense row matrix x sparse matrix performance tests"};
+    std::size_t NR;
+    app.add_option("-r,--row", NR, "Number of matrix rows")->default_val(10000);
+    std::size_t NC;
+    app.add_option("-c,--column", NC, "Number of matrix columns")->default_val(5000);
+    int iterations;
+    app.add_option("-i,--iter", iterations, "Number of iterations")->default_val(10);
+    unsigned long long seed;
+    app.add_option("-s,--seed", seed, "Seed for the simulated data")->default_val(69);
+    CLI11_PARSE(app, argc, argv);
+
+    // Simulating a row-major sparse matrix.
+    std::mt19937_64 rng(seed);
+    std::vector<std::vector<FLOAT> > mat_value(NR);
+    std::vector<std::vector<int> > mat_index(NR);
+    std::uniform_real_distribution<> udist;
+    std::normal_distribution<> dist;
+    for (std::size_t r = 0; r < NR; ++r) {
+        for (std::size_t c = 0; c < NC; ++c) {
+            if (udist(rng) <= 0.1) {
+                mat_index[r].push_back(c);
+                mat_value[r].push_back(dist(rng));
+            }
+        }
+        mat_index[r].shrink_to_fit();
+        mat_value[r].shrink_to_fit();
+    }
+
+    // Simulating the RHS vector.
+    std::vector<FLOAT> rhs(NC);
+    for (std::size_t c = 0; c < NC; ++c) {
+        rhs[c] = dist(rng);
+    }
+
+    std::vector<std::function<FLOAT()> > funs;
+    std::vector<std::string> names;
+    funs.reserve(20);
+    names.reserve(20);
+
+    // Naive multiplication.
+    std::vector<FLOAT> naive(NR);
+    names.emplace_back("naive");
+    funs.emplace_back([&]() -> FLOAT {
+        for (std::size_t r = 0; r < NR; ++r) {
+            naive[r] = dense_sparse_dot_product<1>(mat_value[r], mat_index[r], rhs);
+        }
+        return naive.front() + naive.back();
+    });
+
+    // Multiple accumulators.
+    std::vector<FLOAT> ma_2(NR);
+    names.emplace_back("2 accumulators");
+    funs.emplace_back([&]() -> FLOAT {
+        for (std::size_t r = 0; r < NR; ++r) {
+            ma_2[r] = dense_sparse_dot_product<2>(mat_value[r], mat_index[r], rhs);
+        }
+        return ma_2.front() + ma_2.back();
+    });
+
+    std::vector<FLOAT> ma_4(NR);
+    names.emplace_back("4 accumulators");
+    funs.emplace_back([&]() -> FLOAT {
+        for (std::size_t r = 0; r < NR; ++r) {
+            ma_4[r] = dense_sparse_dot_product<4>(mat_value[r], mat_index[r], rhs);
+        }
+        return ma_4.front() + ma_4.back();
+    });
+
+    std::vector<FLOAT> ma_8(NR);
+    names.emplace_back("8 accumulators");
+    funs.emplace_back([&]() -> FLOAT {
+        for (std::size_t r = 0; r < NR; ++r) {
+            ma_8[r] = dense_sparse_dot_product<4>(mat_value[r], mat_index[r], rhs);
+        }
+        return ma_8.front() + ma_8.back();
+    });
+
+    // Performing the iterations.
+    eztimer::Options opt;
+    opt.iterations = iterations;
+
+    std::optional<FLOAT> expected;
+    auto res = eztimer::time<FLOAT>(
+        funs,
+        [&](const FLOAT& res, std::size_t i) -> void {
+            if (expected.has_value()) {
+#ifndef USE_SINGLE
+                if (std::abs(*expected - res) > 1e-8) {
+#else
+                if (std::abs(*expected - res) > 1e-2) {
+#endif
+                    throw std::runtime_error("oops that's not right");
+                }
+            } else {
+                expected = res;
+            }
+        },
+        opt
+    );
+
+    std::cout << "Results for " << NR << " x " << NC << std::endl;
+    const std::size_t num_methods = names.size();
+    for (std::size_t m = 0; m < num_methods; ++m) {
+        auto name = names[m];
+        if (name.size() < 50) {
+            name.insert(name.end(), 50 - name.size(), ' ');
+        }
+        std::cout << name << ": " << res[m].mean.count() << " ± " << res[m].sd.count() / std::sqrt(res[m].times.size()) << std::endl;
+    }
+
+    return 0;
+}
