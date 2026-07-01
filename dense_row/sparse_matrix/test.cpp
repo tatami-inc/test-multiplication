c576e5e503b0afa13171b592c442dfe57eca153a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-07-01 16:36:38 +1000

    Reuse function definition of the dense-sparse dot product.

--- dense_row/sparse_matrix/test.cpp
+++ dense_row/sparse_matrix/test.cpp
@@ -13,40 +13,7 @@
 #define FLOAT float
 #endif
 
-template<int num_acc_, class ValueIterator_, class IndexIterator_, class Dense_>
-FLOAT dense_sparse_dot_product(const std::size_t num_non_zeros, const ValueIterator_& value_ptr, const IndexIterator_& index_ptr, const Dense_& dense, FLOAT initial) {
-    if constexpr(num_acc_ == 1) {
-        FLOAT dot = initial;
-        for (std::size_t i = 0; i < num_non_zeros; ++i) {
-            dot += dense[*(index_ptr + i)] * *(value_ptr + i);
-        }
-        return dot;
-
-    } else {
-        std::array<FLOAT, num_acc_> dots{};
-        const std::size_t cycles = num_non_zeros / num_acc_;
-        const std::size_t remainder = num_non_zeros % num_acc_;
-
-        for (std::size_t c = 0; c < cycles; ++c) {
-            for (std::size_t i = 0; i < num_acc_; ++i) {
-                const auto idx = c * num_acc_ + i;
-                dots[i] += dense[*(index_ptr + idx)] * *(value_ptr + idx);
-            }
-        }
-
-        FLOAT extras = initial;
-        for (std::size_t i = 0; i < remainder; ++i) {
-            const auto idx = cycles * num_acc_ + i;
-            extras += dense[*(index_ptr + idx)] * *(value_ptr + idx);
-        }
-        return std::accumulate(dots.begin(), dots.end(), extras);
-    }
-}
-
-template<int num_acc_, class Values_, class Indices_, class Dense_>
-FLOAT dense_sparse_dot_product(const Values_& values, const Indices_& indices, const Dense_& dense) {
-    return dense_sparse_dot_product<num_acc_>(values.size(), values.begin(), indices.begin(), dense, 0);
-}
+#include "dense_sparse_dot_product.h"
 
 template<int num_acc_, bool output_columnar_>
 void blocked_mult_with_right_column(
